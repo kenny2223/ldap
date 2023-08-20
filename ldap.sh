b71c51584f20e614462fcd12b6d311553012dfdc
kenny2223 <38969256+kenny2223@users.noreply.github.com>
2023-08-20 16:45:27 -0600

    Add files via upload

---
+++ ldap.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+pass="sen31994"
+
+
+wget https://github.com/kenny2223/ldap/blob/main/issabel-ldap-1.0.0.tgz?raw=true -O issabel-ldap-1.0.0.tgz
+
+tar -xvzf issabel-ldap-1.0.0.tgz
+cd issabel-ldap-1.0.0
+
+echo -e "$pass\n$pass\n" | make install 
+
+chmod 666  /etc/sysconfig/issabel-ldap
+
+cd ..
+
+wget https://github.com/kenny2223/ldap/blob/main/ldap.tar.gz?raw=true -O ldap.tar.gz
+
+tar -xvzf ldap.tar.gz
+
+cd ldap
+
+#make the menu.xml
+cat << EOF > menu.xml 
+<?xml version="1.0" encoding="UTF-8"?>
+<module>
+<menulist>
+<menuitem parent="agenda" module="yes" link="" menuid="ldap" desc="Ldap" order="3" />
+</menulist>
+</module>
+EOF
+
+issabel-menumerge ./menu.xml
+rm -f ./menu.xml
+
+#Copy  all modules to /var/www/html/modules/ de issabell
+cp -r ./Modulos/* /var/www/html/modules/
+
+cd ..
+
+rm -rf ldap*
+rm -rf issabel-ldap*
+
+
+
+
